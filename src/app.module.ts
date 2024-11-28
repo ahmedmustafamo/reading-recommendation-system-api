da9a3f4a5c3db197618fac1310a7848776c62386
Ahmed Mustafa <59970699+ahmedmustafamo@users.noreply.github.com>
2024-11-28 22:56:52 +0000

    enable ConfigModule to be global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
@@ -6,11 +7,12 @@ import { BookModule } from './books/book.module';
 import { ReadingModule } from './readings/reading.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
     UserModule,
     BookModule,
