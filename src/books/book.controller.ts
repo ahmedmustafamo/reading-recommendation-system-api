b0ebdc9aad9c2781f81941f27c515244f33349fb
Ahmed Mustafa <59970699+ahmedmustafamo@users.noreply.github.com>
2024-11-29 08:44:34 +0000

    feat: add role based on create/update book

--- src/books/book.controller.ts
+++ src/books/book.controller.ts
@@ -1,17 +1,23 @@
-import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/RoleGuards';
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) { }
 
   @Post()
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   async createBook(@Body() createBookDto: CreateBookDto) {
     return this.bookService.createBook(createBookDto);
   }
 
   @Put(':id')
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   async updateBook(
     @Param('id') id: number,
     @Body() updateData: Partial<CreateBookDto>) {
@@ -19,6 +25,8 @@ export class BookController {
   }
 
   @Get()
+  @Roles('admin', 'user')
+  @UseGuards(RolesGuard)
   async getAllBooks() {
     return this.bookService.getAllBooks();
   }
--- src/books/book.module.ts
+++ src/books/book.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BookService],
+  providers: [BookService, JwtService],
   controllers: [BookController],
 })
 export class BookModule {}
